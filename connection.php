7d44d9d1cdfe2527071584ae50628403c1827687
ALSHIMAAA <87203152+ALSHIMAAA@users.noreply.github.com>
2022-08-16 10:45:23 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,21 +1,21 @@
 <?php
 
-	$dsn='mysql:host=localhost;dbname=beautydb'; // مصدر قاعدة البيانات : اسم قاعده البيانات و اسم السرفر
-	$user='root'; // اسم المستخدم للاتصال بقاعده البيانات وهو اسم افتراضي
-	$pass=''; // كلمة السر للاتصال بقاعده البيانات وهي افتراضية
-	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);// ترميز قاعده البيانات لتدعم اللغه العربية
+	$dsn='mysql:host=localhost;dbname=beautydb'; 
+	$user='root'; 
+	$pass=''; 
+	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
   
 	try{
 	   
-	    $conn=new PDO($dsn,$user,$pass,$options); // البدء ب الاتصال بقاعده البيانات باستخدام كلاس بي دي او هذا الكلاس موجود ضمن لغة بي اتش بي 
-        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); // في حاله حصول خطاء في الاتصال
+	    $conn=new PDO($dsn,$user,$pass,$options); 
+        $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION); 
         
        
 	}
 	
 	catch(PDOExcepction $error) {
 	    
-	    echo "error".$error->getMessage(); // عرض رساله الخطاء في حاله حصول خطاء في الاتصال بقاعدة البيانات
+	    echo "error".$error->getMessage(); 
 	}
 
 ?>
